045bbb9132c8ec0a636723a7f217f34d2aac72a6
Daniel Tendero García <danieltendero@hotmail.es>
2022-02-05 00:12:25 +0100

    Connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,5 @@
-console.log("Test");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/mongo-test", {useNewUrlParser: true})
+.then(() => console.log("Connected"))
+.catch(err => console.log(err));
