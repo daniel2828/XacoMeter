d20b9b043d2f07c266627cc046ecc9c770603289
Daniel Tendero García <danieltendero@hotmail.es>
2022-02-07 07:40:32 +0100

    Added tests with Sentiment Analysis libraries

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/mongo-test", {useNewUrlParser: true})
-.then(() => console.log("Connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongo-test", { useNewUrlParser: true })
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+// var Sentiment = require('sentiment');
+// var sentiment = new Sentiment();
+// var result = sentiment.analyze('Pepe is a fucking shit.');
+// console.dir(result);
+
+
+const lorca = require('lorca-nlp');
+
+var doc = lorca('esto es un test');
+
+doc.words().get();
+// [ 'esto', 'es', 'un', 'test' ]
+var doc = lorca('El plátano está malo.');
+
+console.log(doc.sentiment());
+// -0.75
+
+var doc = lorca('El camino De Santiago,a su paso por la Ruta Signo de registrado importante ser felices');
+
+console.log(doc.sentiment());
