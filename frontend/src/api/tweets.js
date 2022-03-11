a293d6f5008c5ae1eda4f948b6507f67c0b89193
Daniel Tendero García <danieltendero@hotmail.es>
2022-03-11 15:32:49 +0100

    Add css pages

---
+++ frontend/src/api/tweets.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { BASE_PATH, API_VERSION } from "./config";
+export const getTweetsByHashtag = async (hashtag)=>{
+   const data = await axios.post(`${BASE_PATH}/${API_VERSION}/twitter/searchByQuery`)
+   console.log(data);
+   return data;
+}
