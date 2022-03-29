6c9b2203ec75a6301725d104c09e1e1f16203139
Daniel Tendero García <danieltendero@hotmail.es>
2022-03-29 19:49:04 +0200

    #29 - Add securize to the endpoints

--- backend/routes/twitterApi.js
+++ backend/routes/twitterApi.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
 const TwitterController = require("../controllers/twitterApi");
-
+const md_auth = require("../middlewares/authenticated");
 const api = express.Router();
 
 api.post("/test", TwitterController.testAPI);
-api.post("/searchByQuery", TwitterController.searchByQuery);
+api.post("/searchByQuery",[md_auth.ensureAuth], TwitterController.searchByQuery);
 //api.get("/getTweetsByHashtag", TwitterController.getTweetsByHashtag);
 module.exports = api;
--- frontend/src/api/tweets.js
+++ frontend/src/api/tweets.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { BASE_PATH, API_VERSION } from "./config";
-export const getTweetsByHashtag = async (hashtag)=>{
-   const data = await axios.post(`${BASE_PATH}/${API_VERSION}/twitter/searchByQuery`,{hashtag})
+export const getTweetsByHashtag = async (hashtag,accessToken)=>{
+   const data = await axios.post(`${BASE_PATH}/${API_VERSION}/twitter/searchByQuery`,{hashtag},{headers:{
+      "Content-Type": "application/json",
+      Authorization : accessToken
+   }})
    console.log(data);
    return data;
 }
