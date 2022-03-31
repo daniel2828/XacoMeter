3132cb983028f08722b34ad78c05933e0f0031bc
Daniel Tendero García <danieltendero@hotmail.es>
2022-03-31 13:48:50 +0200

    #27 Prepare frontend for deployment

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,2 +1,2 @@
-export const BASE_PATH = `http://localhost:${process.env.PORT}/api`;
+export const BASE_PATH = process.env.BACK_PATH || `http://localhost:${process.env.PORT}/api`;
 export const API_VERSION = "v1";
