7874b313137597d43b17c8576d3a1bbb5273ae8c
Daniel Tendero García <danieltendero@hotmail.es>
2022-03-31 15:51:35 +0200

    #27 Prepare frontend for deployment

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { BASE_PATH, API_VERSION } from "./config";
 export const signIn =async (body)=>{
+   console.log(process.env.BACK_PAT)
    const data = await axios.post(`${BASE_PATH}/${API_VERSION}/users/sign-in`, body)
    console.log(data);
    return data;
