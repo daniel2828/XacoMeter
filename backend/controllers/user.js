6cc8c3186899e526b144dbe0f0da32074c8dfa24
Daniel Tendero García <danieltendero@hotmail.es>
2022-06-14 11:44:23 +0200

    #89 - responsive finished

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -134,13 +134,13 @@ async function passRecovery(req, res) {
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "white.sharks.produtions@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  });
+  // let info = await transporter.sendMail({
+  //   from: '"Fred Foo 👻" <foo@example.com>', // sender address
+  //   to: "white.sharks.produtions@gmail.com", // list of receivers
+  //   subject: "Hello ✔", // Subject line
+  //   text: "Hello world?", // plain text body
+  //   html: "<b>Hello world?</b>", // html body
+  // });
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
