524f503e220ea6332e33b35493fbfeb96388028b
Daniel Tendero García <danieltendero@hotmail.es>
2022-06-14 11:45:47 +0200

    #89 - responsive finished

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -123,15 +123,15 @@ async function passRecovery(req, res) {
   let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+  // let transporter = nodemailer.createTransport({
+  //   host: "smtp.ethereal.email",
+  //   port: 587,
+  //   secure: false, // true for 465, false for other ports
+  //   auth: {
+  //     user: testAccount.user, // generated ethereal user
+  //     pass: testAccount.pass, // generated ethereal password
+  //   },
+  // });
 
   // send mail with defined transport object
   // let info = await transporter.sendMail({
